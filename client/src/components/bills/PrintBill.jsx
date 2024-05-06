a363ac9d5d5c1f541e3ca8063f4024da17ac4f6d
unsatisfiedcale <unsatisfiedcale@gmail.com>
2024-05-07 01:32:46 +0300

    PrintBill

--- client/src/components/bills/PrintBill.jsx
+++ client/src/components/bills/PrintBill.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Select } from "antd";
+import { Button, Modal, Select } from "antd";
 
 const PrintBill = ({ isModalOpen, setIsModalOpen }) => {
   return (
@@ -9,7 +9,6 @@ const PrintBill = ({ isModalOpen, setIsModalOpen }) => {
       footer={false}
       onCancel={() => setIsModalOpen(false)}
       width={800}
-      
     >
       <section className="py-20 bg-black">
         <div className="max-w-5xl max-auto bg-white px-6">
@@ -18,7 +17,7 @@ const PrintBill = ({ isModalOpen, setIsModalOpen }) => {
               <h2 className="text-4xl font-bold text-slate-700 my-6">LOGO</h2>
             </div>
             <div className="bill-details">
-              <div className="grid grid-cols-4 gap-12">
+              <div className="grid sm:grid-cols-4 grid-cols-3 gap-12">
                 <div className="text-md text-slate-500">
                   <p className="font-bold ">Fatura Detayı: </p>
                   <p>Unwrapped</p>
@@ -35,29 +34,136 @@ const PrintBill = ({ isModalOpen, setIsModalOpen }) => {
                 </div>
                 <div className="text-md text-slate-500">
                   <div>
-                  <p className="font-bold ">Fatura Numarası: </p>
-                  <p>00041</p>
+                    <p className="font-bold ">Fatura Numarası: </p>
+                    <p>00041</p>
                   </div>
                   <div>
-                  <p className="font-bold ">Veriliş Tarihi: </p>
-                  <p>04-03-2024</p>
+                    <p className="font-bold ">Veriliş Tarihi: </p>
+                    <p>04-03-2024</p>
                   </div>
                 </div>
-                <div className="text-md text-slate-500">
+                <div className="text-md text-slate-500 sm:block hidden">
                   <div>
-                  <p className="font-bold ">Şartlar </p>
-                  <p>10 Gün</p>
+                    <p className="font-bold ">Şartlar </p>
+                    <p>10 Gün</p>
                   </div>
                   <div>
-                  <p className="font-bold ">Vade: </p>
-                  <p>17-06-2024</p>
+                    <p className="font-bold ">Vade: </p>
+                    <p>17-06-2024</p>
                   </div>
+                </div>
+              </div>
+            </div>
+            <div className="bill-table-area mt-8">
+              <table className="min-w-full divide-y divide-slate-500 overflow-hidden">
+                <thead>
+                  <tr className="border-b border-slate-200">
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 text-left text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden"
+                    >
+                      Görsel
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 text-left text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden"
+                    >
+                      Başlık
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 text-left text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden"
+                    >
+                      Fiyat
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 text-center text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden"
+                    >
+                      Adet
+                    </th>
+                    <th
+                      scope="col"
+                      className="py-3.5 pl-4 text-end text-sm font-normal text-slate-700 sm:pl-6 md:pl-0 sm:table-cell hidden"
+                    >
+                      Toplam
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="border-b border-slate-200">
+                    <td className="py-4 pr-3">
+                      <img
+                        src="https://i.lezzet.com.tr/images-xxlarge-secondary/elma-nasil-yenir-221135ca-f383-474c-a4f5-ad02a45db978.jpg"
+                        alt=""
+                        className="w-12 h-12 object-fill"
+                      ></img>
+                    </td>
+                    <td className="py-4 pr-3">
+                      <span className="font-medium">Elma</span>
+                    </td>
+                    <td className="py-4 pr-3">
+                      <span className="font-medium">5₺</span>
+                    </td>
+                    <td className="py-4 pr-3 text-center">
+                      <span className="font-medium">1</span>
+                    </td>{" "}
+                    <td className="py-4 pr-3 text-end">
+                      <span className="font-medium">1</span>
+                    </td>
+                  </tr>
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <th className="text-right pt-6" colSpan="4" scope="row">
+                      <span className="font-normal text-slate-700">
+                        Ara Toplam
+                      </span>
+                    </th>
+                    <th className="text-right pt-6" colSpan="4" scope="row">
+                      <span className="font-normal text-slate-700">53 ₺</span>
+                    </th>
+                  </tr>
+                  <tr>
+                    <th className="text-right pt-6" colSpan="4" scope="row">
+                      <span className="font-normal text-slate-700">KDV</span>
+                    </th>
+                    <th className="text-right pt-6" colSpan="4" scope="row">
+                      <span className="font-normal text-red-700">+4.88</span>
+                    </th>
+                  </tr>
+                  <tr>
+                    <th className="text-right pt-6" colSpan="4" scope="row">
+                      <span className="font-normal text-slate-700">Toplam</span>
+                    </th>
+                    <th className="text-right pt-6" colSpan="4" scope="row">
+                      <span className="font-normal text-slate-700">57.88 ₺</span>
+                    </th>
+                  </tr>
+                </tfoot>
+              </table>
+              <div className="py-9">
+                <div className="border-t pt-9 border-slate-300">
+                <p className="text-sm font-light text-slate-700">
+                  Ödeme koşulları 14 gündür. Paketlenmemiş Borçların Geç
+                  Ödenmesi Yasası 0000'e göre, serbest çalışanların bu süreden
+                  sonra borçların ödenmemesi durumunda 00.00 gecikme ücreti
+                  talep etme hakkına sahip olduklarını ve bu noktada bu ücrete
+                  ek olarak yeni bir fatura sunulacağını lütfen unutmayın.
+                  Revize faturanın 14 gün içinde ödenmemesi durumunda, vadesi
+                  geçmiş hesaba ek faiz ve %8 yasal oran artı %0,5 Bank of
+                  England tabanı olmak üzere toplam %8,5 uygulanacaktır.
+                  Taraflar Kanun hükümleri dışında sözleşme yapamazlar.
+                </p>
                 </div>
               </div>
             </div>
           </article>
         </div>
       </section>
+      <div className="flex justify-end mt-4">
+        <Button type="primary" size="large">Yazdır</Button>
+      </div>
     </Modal>
   );
 };
